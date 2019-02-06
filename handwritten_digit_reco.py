25c5b574844976b1ae6fc574899ca828e4c4eaa2
namankhetan <42545376+namankhetan@users.noreply.github.com>
2019-02-07 02:44:03 +0530

    Add files via upload

---
+++ handwritten_digit_reco.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 20:25:43 2019
+
+@author: Tushar
+"""
+
+from keras.datasets import mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+import matplotlib.pyplot as plt
+plt.imshow(X_train[0])
+
+X_train[0].shape
+
+X_train = X_train.reshape(60000,28,28,1)
+X_test = X_test.reshape(10000,28,28,1)
+from keras.utils import to_categorical
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+y_train[0]
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+
+model = Sequential()
+
+model.add(Conv2D(128, kernel_size=3, activation='relu', input_shape=(28,28,1)))
+model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Conv2D(16, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(70, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3)
+
+y_pred = model.predict(X_test)
+score = model.evaluate(x_test, y_test, verbose=0)
