65f8aaa72ed24ed8509b08aa791c645d9c23da27
hulkier <47470892+hulkier@users.noreply.github.com>
2019-03-03 23:53:58 +0530

    Update handwritten_digit_reco.py

--- handwritten_digit_reco.py
+++ handwritten_digit_reco.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 31 20:25:43 2019
-
-@author: Tushar
-"""
+#
 
 from keras.datasets import mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
